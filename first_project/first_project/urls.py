a76d2e1edc04e2423898b7159f6d34d8641fd0cb
Liberbell <liberbell@gmail.com>
2022-02-09 15:35:21 +0900

    urls2

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first_app1/', include('first_app1.urls'))
+    path('app1/', include('first_app1.urls'))
 ]
