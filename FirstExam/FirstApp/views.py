56d4e0c4975c35b9318c57bf9680661ebe19a19e
Liberbell <liberbell@gmail.com>
2022-02-09 16:01:24 +0900

    vies4

--- FirstExam/FirstApp/views.py
+++ FirstExam/FirstApp/views.py
@@ -8,4 +8,4 @@ def add(request, num1, num2):
 def minus(request, num1, num2):
     num1 = float(num1)
     num2 = float(num2)
-    return HttpResponse()
+    return HttpResponse(f'<h1>{num1 - num2}</h1>')
