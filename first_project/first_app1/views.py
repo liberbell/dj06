f7fb9dada3b8ebeaff6739673570c366c46d1d1f
Liberbell <liberbell@gmail.com>
2022-02-09 15:53:36 +0900

    views8

--- first_project/first_app1/views.py
+++ first_project/first_app1/views.py
@@ -9,4 +9,5 @@ def user_page(request, user_name):
     return HttpResponse(f'<h1>{user_name}\'s page</h1>')
 
 def number_page(request, user_name, number):
+    user_name = user_name.upper()
     return HttpResponse(f'<h1>{user_name}\'s page number = {number}</h1>')
