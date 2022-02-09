02e20693e83b3670c5f87e880ab97758870ff8eb
Liberbell <liberbell@gmail.com>
2022-02-09 15:51:23 +0900

    urls6

--- first_project/first_app1/urls.py
+++ first_project/first_app1/urls.py
@@ -8,5 +8,5 @@ app_name = 'first_app1'
 urlpatterns = [
     path('hello', views.index, name='index'),
     path('page/<str:user_name>', views.user_page, name='user_page'),
-    path('number_page/<int:number>', views.number_page, name='number_page'),
+    path('number_page/<str:user_name>/<int:number>', views.number_page, name='number_page'),
 ]
