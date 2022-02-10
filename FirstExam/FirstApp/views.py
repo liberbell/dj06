dee4861bb3ace2f627d5f974ab709dd6759a4daf
Liberbell <liberbell@gmail.com>
2022-02-10 13:59:02 +0900

    views3

--- FirstExam/FirstApp/views.py
+++ FirstExam/FirstApp/views.py
@@ -13,4 +13,4 @@ def minus(request, num1, num2):
 def div(request, num1, num2):
     num1 = float(num1)
     num2 = float(num2)
-    return HttpResponse(f'<h1>{num1 / num2}</h1>')
+    return HttpResponse(f'<h1>{round(num1 / num2)}</h1>')
