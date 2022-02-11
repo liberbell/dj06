6f7f50ce443b7ee2068bf10de3d37cd3600dd0a8
Liberbell <liberbell@gmail.com>
2022-02-11 15:26:16 +0900

    views3

--- samptemp/TemplateProject/TemplateApp/views.py
+++ samptemp/TemplateProject/TemplateApp/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def index(request):
     val = "This is the value input."
     return render(request, 'TemplateApp/index.html', context={'value': val})
+
+def home(request):
+    my_name = "Eric Crapton"
+    favorite_fruit = ["Apple", "Grape", "Lemon"]
+    my_info = {
+        "name": "Eric",
+        "age": 70
+    }
+
+    return render(request, )
