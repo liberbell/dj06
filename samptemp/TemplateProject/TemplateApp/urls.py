6147737478bd22c0d1d889a8bf47321f7e9e7287
Liberbell <liberbell@gmail.com>
2022-02-12 14:39:02 +0900

    urls4

--- samptemp/TemplateProject/TemplateApp/urls.py
+++ samptemp/TemplateProject/TemplateApp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name="home"),
     path("sample1", views.sample1, name="sample1"),
-    path("sample2", views.sample2, name="sample2")
+    path("sample2", views.sample2, name="sample2"),
+    path("sample", views.sample, name="sample")
 ]
