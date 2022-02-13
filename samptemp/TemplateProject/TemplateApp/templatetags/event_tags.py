11a5bda903a9dd364a1168d8c6c072714242a5bb
Liberbell <liberbell@gmail.com>
2022-02-13 13:11:34 +0900

    eventtag3

--- samptemp/TemplateProject/TemplateApp/templatetags/event_tags.py
+++ samptemp/TemplateProject/TemplateApp/templatetags/event_tags.py
@@ -3,3 +3,11 @@ from django import Template
 
 register = template.Library()
 @register.filter(name="status_to_string")
+
+def convert_status_to_string(status):
+    if status == 10:
+        return "success"
+    elif status == 20:
+        return "error"
+    elif status == 30:
+        return "pending"
