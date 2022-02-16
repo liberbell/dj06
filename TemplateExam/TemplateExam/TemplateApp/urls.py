89fb74a635293a8267165dff32c0fa530f8a04cd
Liberbell <liberbell@gmail.com>
2022-02-17 08:59:00 +0900

    urls1

--- TemplateExam/TemplateExam/TemplateApp/urls.py
+++ TemplateExam/TemplateExam/TemplateApp/urls.py
@@ -7,5 +7,5 @@ app_name = "app"
 urlpatterns = [
     path("home", views.home, name="home"),
     path("members", views.members, name="members"),
-    path("member/<int:id>", views.members, name="members"),
+    path("member/<int:id>", views.member, name="member"),
 ]
