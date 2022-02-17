20049ff5c9dc3a917d8b1d2bbe5c173eead3f514
Liberbell <liberbell@gmail.com>
2022-02-18 08:54:46 +0900

    views1

--- TemplateExam/TemplateExam/TemplateApp/views.py
+++ TemplateExam/TemplateExam/TemplateApp/views.py
@@ -11,10 +11,10 @@ class Member:
         self.picture_path = picture_path
 
 member_list = [
-    Member(0, "Bob", "2022/2/1", "img/Bob.jpg"),
-    Member(1, "Eric", "2022/2/4", "img/eric.jpg"),
-    Member(2, "Alex", "2022/2/5", "img/alex.jpg"),
-    Member(3, "Elton", "2022/2/10", "img/elton.jpg")
+    Member(0, "Bob", "2021/2/1", "img/Bob.jpg"),
+    Member(1, "Eric", "2020/2/4", "img/eric.jpg"),
+    Member(2, "Alex", "2022/1/5", "img/alex.jpg"),
+    Member(3, "Elton", "2021/12/10", "img/elton.jpg")
 ]
 
 def home(request):
