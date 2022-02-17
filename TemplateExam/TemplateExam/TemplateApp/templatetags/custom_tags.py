584e6b91c2a3d6480d4425809620bf5e1803d263
Liberbell <liberbell@gmail.com>
2022-02-18 08:37:18 +0900

    customtags2

--- TemplateExam/TemplateExam/TemplateApp/templatetags/custom_tags.py
+++ TemplateExam/TemplateExam/TemplateApp/templatetags/custom_tags.py
@@ -1,8 +1,11 @@
 from atexit import register
 from django import template
+from datetime import datetime
 
 register = template.library()
 
 @register.filter(name="calculate_datetime_to_now")
 def calculate_datetime_to_now(value):
-    
+    join_datetime = datetime.strptime(value, "%Y%m%d")
+    now_datetime = datetime.now()
+    diff_datetime = now_datetime - join_datetime
