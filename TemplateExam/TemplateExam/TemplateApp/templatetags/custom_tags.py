4bf6b0fa9c2d2aec3beb9680bc6177f17cd9a51f
Liberbell <liberbell@gmail.com>
2022-02-18 08:38:46 +0900

    customtags4

--- TemplateExam/TemplateExam/TemplateApp/templatetags/custom_tags.py
+++ TemplateExam/TemplateExam/TemplateApp/templatetags/custom_tags.py
@@ -1,6 +1,7 @@
 from atexit import register
 from django import template
 from datetime import datetime
+import math
 
 register = template.library()
 
@@ -10,4 +11,4 @@ def calculate_datetime_to_now(value):
     now_datetime = datetime.now()
     diff_datetime = now_datetime - join_datetime
     diff_days = diff_datetime.days
-    
+    diff_years = math.floor(diff_days / 365)
