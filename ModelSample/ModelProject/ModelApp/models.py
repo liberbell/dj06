bd1cb83b7aa278a51cb30372c9188064f18164d4
Liberbell <liberbell@gmail.com>
2022-02-19 13:55:09 +0900

    models4

--- ModelSample/ModelProject/ModelApp/models.py
+++ ModelSample/ModelProject/ModelApp/models.py
@@ -5,4 +5,5 @@ class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     birthday = models.DateField(default="1900-01-01")
+    email = models.EmailField(db_index=True)
 
