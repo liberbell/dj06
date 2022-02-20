96237bf13a7dc18eca92f71ad62f3b2ed50b1a38
Liberbell <liberbell@gmail.com>
2022-02-20 13:23:33 +0900

    main3

--- ModelSample/ModelProject/main.py
+++ ModelSample/ModelProject/main.py
@@ -5,3 +5,9 @@ from django import setup
 setup()
 
 from ModelApp.models import Person
+
+p = Person(
+    first_name = "Bob", last_name = "Mary", birthday = 1960/7/1,
+    email = "bob@example.com", salary = 300000, memo = "bob memo",
+    web_site = "www.google.com"
+)
