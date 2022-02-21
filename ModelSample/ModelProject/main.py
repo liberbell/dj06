1633783a9ef3d70a43b369ab44c933b34eadf056
Liberbell <liberbell@gmail.com>
2022-02-21 22:39:32 +0900

    main7

--- ModelSample/ModelProject/main.py
+++ ModelSample/ModelProject/main.py
@@ -27,7 +27,7 @@ p = Person(
 
 obj, created = Person.objects.get_or_create(
     first_name="Elton", last_name="Jhon",
-    email="eric@example.com", salary=150000, memo="class method",
+    email="eric@example.com", salary=250000, memo="class method",
     web_site="None"
 )
 print(obj)
