078073d8b360e4cfba4f83a28aa6aad7853f94c4
Liberbell <liberbell@gmail.com>
2022-02-22 22:49:27 +0900

    updatesample2

--- ModelSample/ModelProject/update_sample.py
+++ ModelSample/ModelProject/update_sample.py
@@ -5,3 +5,9 @@ from django import setup
 setup()
 
 from ModelApp.models import Person
+from django.utils import timezone
+import pytz
+
+person = Person.objects.get(id=1)
+person.birsthday = "2001-01-01"
+person.save()
