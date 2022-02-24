0edae5286bef62433fa807614b1b544ad1bb6329
Liberbell <liberbell@gmail.com>
2022-02-24 21:46:44 +0900

    foreignkeysample3

--- ModelSample/ModelProject/foreignkey_sample.py
+++ ModelSample/ModelProject/foreignkey_sample.py
@@ -6,3 +6,7 @@ from django import setup
 setup()
 
 from ModelApp.models import Students, Schools, Prefectures
+
+prefectures = ["Tokyo", "Osaka"]
+schools = ["West side School", "South side School", "East side School", "North side School"]
+students = ["Alex", "Bob", "Elton", "Eric"]
