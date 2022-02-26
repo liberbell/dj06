37a9269f4984464aad98a7170946d059af12985a
Liberbell <liberbell@gmail.com>
2022-02-26 23:04:12 +0900

    models2

--- ModelSample/ModelProject/ModelApp/models.py
+++ ModelSample/ModelProject/ModelApp/models.py
@@ -34,7 +34,7 @@ class Students(models.Model):
     age = models.IntegerField()
     major = models.CharField(max_length=20)
     school = models.ForeignKey(
-        "Schools", on_delete=models.SET_NULL, null=True
+        "Schools", on_delete=models.RESTRICT
     )
 
     class Meta:
@@ -43,7 +43,7 @@ class Students(models.Model):
 class Schools(models.Model):
     name = models.CharField(max_length=20)
     prefecture = models.ForeignKey(
-        "Prefectures", on_delete=models.SET_NULL, null=True
+        "Prefectures", on_delete=models.RESTRICT
     )
 
     class Meta:
