983a40fab381c5981cf9cea172da457e8b2fb50e
Liberbell <liberbell@gmail.com>
2022-02-27 19:16:15 +0900

    foreignkeysample1

--- ModelSample/ModelProject/foreignkey_sample.py
+++ ModelSample/ModelProject/foreignkey_sample.py
@@ -25,7 +25,7 @@ def insert_records():
             school.save()
             for student_name in students:
                 student = Students(
-                    name=student_name, age=17, major="Science", school=school
+                    name=student_name, age=17, major="Science", prefecture=prefectures, school=school
                 )
                 student.save()
 
@@ -35,7 +35,7 @@ def select_students():
     for student in students:
         print(student.id, student.name, student.school.id, student.school.name, student.school.prefecture.id, student.school.prefecture.name)
 
-# insert_records()
-# select_students()
+insert_records()
+select_students()
 # Schools.objects.filter(id=1).delete()
 # Prefectures.objects.filter(id=1).delete()
