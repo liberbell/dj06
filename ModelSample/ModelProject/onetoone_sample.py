ef5a0baebd4361500b598fca492e9dd915d5190c
Liberbell <liberbell@gmail.com>
2022-02-27 19:59:12 +0900

    onetoonesample4

--- ModelSample/ModelProject/onetoone_sample.py
+++ ModelSample/ModelProject/onetoone_sample.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from email.headerregistry import Address
+from importlib.util import resolve_name
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ModelProject.settings")
@@ -15,3 +16,6 @@ restaurants = ["Lick `N` hick`N", "Sigilgaida"]
 
 for place_name, place_address in places:
     p = Places(name=place_name, address=place_address)
+    p.save()
+    for restaurant_name in restaurants:
+        r = Restaurants(name=restaurant_name)
