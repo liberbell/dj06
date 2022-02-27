340b9280fb448c34e7729bfb2c04a28a0d3a79de
Liberbell <liberbell@gmail.com>
2022-02-27 19:44:58 +0900

    onetoonesample2

--- ModelSample/ModelProject/onetoone_sample.py
+++ ModelSample/ModelProject/onetoone_sample.py
@@ -5,4 +5,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ModelProject.settings")
 from django import setup
 setup()
 
-from ModelApp.models import Person
+from ModelApp.models import Places, Restaurants
+
+places = [
+    ("West Brompton", "Earl's Court", "Gloucester Road")
+]
+
