a873b45bd6f5d63faad29dc97a845fbf9d2c3fbf
Liberbell <liberbell@gmail.com>
2022-02-27 19:24:54 +0900

    django5

--- ModelSample/ModelProject/foreignkey_sample.py
+++ ModelSample/ModelProject/foreignkey_sample.py
@@ -35,7 +35,7 @@ def select_students():
     for student in students:
         print(student.id, student.name, student.school.id, student.school.name, student.school.prefecture.id, student.school.prefecture.name)
 
-insert_records()
-select_students()
-# Schools.objects.filter(id=1).delete()
+# insert_records()
+# select_students()
+# Schools.objects.filter(id=2).delete()
 # Prefectures.objects.filter(id=1).delete()
