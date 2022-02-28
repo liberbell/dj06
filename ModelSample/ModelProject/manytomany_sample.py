c9b7c7fe3e282a5a52ed6092152c9121bf9e7f71
Liberbell <liberbell@gmail.com>
2022-02-28 17:03:22 +0900

    manytomanysample3

--- ModelSample/ModelProject/manytomany_sample.py
+++ ModelSample/ModelProject/manytomany_sample.py
@@ -2,9 +2,13 @@ from datetime import datetime
 from email.headerregistry import Address
 from importlib.util import resolve_name
 import os
+from unicodedata import name
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ModelProject.settings")
 from django import setup
 setup()
 
 from ModelApp.models import Books, Authors
+
+book1 = Books(name="BOOK 1")
+book2 = Books(name="BOOK 2")
