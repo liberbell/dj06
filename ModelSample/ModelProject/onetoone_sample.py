b0160e81fd6891f62b0de594d041b4510667c2ef
Liberbell <liberbell@gmail.com>
2022-02-28 16:41:55 +0900

    onetoonesample1

--- ModelSample/ModelProject/onetoone_sample.py
+++ ModelSample/ModelProject/onetoone_sample.py
@@ -12,7 +12,7 @@ from ModelApp.models import Places, Restaurants
 places = [
     ("West Brompton", "London"), ("Ravello", "Salerno")
 ]
-restaurants = ["Lick `N` hick`N", "Sigilgaida"]
+restaurants = ["Lick N hick N", "Sigilgaida"]
 
 for place_name, place_address in places:
     p = Places(name=place_name, address=place_address)
