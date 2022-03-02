949097cb56d18df156cbf7433cc1e39c63570313
Liberbell <liberbell@gmail.com>
2022-03-02 11:16:29 +0900

    query2

--- ModelSample/ModelProject/query_01.py
+++ ModelSample/ModelProject/query_01.py
@@ -7,3 +7,6 @@ from unicodedata import name
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ModelProject.settings")
 from django import setup
 setup()
+
+from ModelApp.models import Students
+print(Students.objects.all())
