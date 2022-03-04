cb6f3ab9f97d7cd0944369fc95a267fdd5e8fff3
Liberbell <liberbell@gmail.com>
2022-03-04 22:55:30 +0900

    models1

--- ModelExam/ModelExam/ModelApp/models.py
+++ ModelExam/ModelExam/ModelApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Classes(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'classes'
