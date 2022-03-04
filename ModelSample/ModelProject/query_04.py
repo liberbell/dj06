48f68e20764c37a105660c70f9cea163f7aedce7
Liberbell <liberbell@gmail.com>
2022-03-04 19:07:03 +0900

    queyr41

---
+++ ModelSample/ModelProject/query_04.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from email.headerregistry import Address
+from importlib.util import resolve_name
+import os
+from unicodedata import name
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ModelProject.settings")
+from django import setup
+setup()
+
+from ModelApp.models import Students, Schools
