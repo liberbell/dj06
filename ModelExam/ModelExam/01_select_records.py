6c504ea8f9b8e459bbe4b48ce046c7840146c389
Liberbell <liberbell@gmail.com>
2022-03-05 13:22:20 +0900

    selectrecords1

---
+++ ModelExam/ModelExam/01_select_records.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ModelExam.settings")
+from django import setup
+setup()
+
+from ModelApp.models import Students
+
+Student = Students.objects.get(pk=1)
