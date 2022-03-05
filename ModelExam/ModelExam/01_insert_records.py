4fd80095af24b158ae5d08fd70985a7f5faf93ba
Liberbell <liberbell@gmail.com>
2022-03-05 13:08:23 +0900

    insertrecord5

--- ModelExam/ModelExam/01_insert_records.py
+++ ModelExam/ModelExam/01_insert_records.py
@@ -8,4 +8,6 @@ from ModelApp.models import Students, TestResults, Tests, Classes
 from random import randint
 
 class_names = ["Class" + c_name for c_name in "ABCDEFGHIJ"]
-print(class_names)
+# print(class_names)
+student_names = ["Student" + s_name for s_name in "ABCDEFGHIJ"]
+test_name = ["Math", "Language", "Science"]
