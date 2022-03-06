45b470cd8d8b4293ea8f576d9891d1e9571d69c6
Liberbell <liberbell@gmail.com>
2022-03-07 08:50:50 +0900

    urls2

--- FormSample/FormSample/FormSample/urls.py
+++ FormSample/FormSample/FormSample/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("form_app", include("FormApp.urls"))
+    path("form_app", include("FormApp.urls")),
 ]
