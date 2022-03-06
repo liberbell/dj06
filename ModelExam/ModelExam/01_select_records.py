c1b90a0014073220bcc7bd5b9162439321ac16b1
Liberbell <liberbell@gmail.com>
2022-03-06 16:16:35 +0900

    selectrecords3

--- ModelExam/ModelExam/01_select_records.py
+++ ModelExam/ModelExam/01_select_records.py
@@ -9,3 +9,12 @@ from ModelApp.models import Students
 student = Students.objects.get(pk=1)
 for test_result in student.testresults_set.all():
     print(student.class_fk.name, student.name, test_result.test.name, test_result.score)
+
+from django.db.models import Sum, Avg, Max, Min
+
+for class_summary in Classes.object.values("name", "students__testresults__test__name").annotate(
+    max_socre = Max("students__testresults__score"),
+    min_socre = Min("students__testresults__score"),
+    avg_socre = Avg("students__testresults__score"),
+    sum_socre = Sum("students__testresults__score"),
+):
