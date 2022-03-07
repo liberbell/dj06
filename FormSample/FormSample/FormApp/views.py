f126363e889f826845fe9666d5ccc3a1b0677af0
Liberbell <liberbell@gmail.com>
2022-03-07 09:30:21 +0900

    views8

--- FormSample/FormSample/FormApp/views.py
+++ FormSample/FormSample/FormApp/views.py
@@ -7,6 +7,11 @@ def index(request):
 
 def form_page(request):
     form = forms.UserInfo()
+    if request.method == "POST":
+        form = forms.UserInfo(request.POST)
+        if form.is_valid:
+            print("Validation Success")
+            print(f"name: {form.cleaned_data["name"]}, mail: {form.cleaned_data["mail"]} ")
     return render(
         request,
         "formapp/form_page.html",
