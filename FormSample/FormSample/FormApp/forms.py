805bace8cf11703906eb81d299a3a3ed0c37175b
Liberbell <liberbell@gmail.com>
2022-03-08 09:01:47 +0900

    forms12

--- FormSample/FormSample/FormApp/forms.py
+++ FormSample/FormSample/FormApp/forms.py
@@ -2,7 +2,7 @@ from logging import PlaceHolder
 from django import forms
 
 class UserInfo(forms.Form):
-    name = forms.CharField(label="Full Name")
+    name = forms.CharField(label="Full Name", max_length=10)
     age = forms.IntegerField(label="Own age")
     mail = forms.EmailField(label="Mail address", widget=forms.TextInput(attrs={"placeholder": "sample@example.com"}))
     is_married = forms.BooleanField(initial=False)
