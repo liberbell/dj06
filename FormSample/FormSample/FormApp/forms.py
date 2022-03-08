71fda0fd91b40311972f4444fde1f6ce2599851c
Liberbell <liberbell@gmail.com>
2022-03-09 08:54:50 +0900

    forms10

--- FormSample/FormSample/FormApp/forms.py
+++ FormSample/FormSample/FormApp/forms.py
@@ -1,10 +1,11 @@
 from logging import PlaceHolder
 from wsgiref.validate import validator
 from django import forms
+from django.core import validators
 
 class UserInfo(forms.Form):
     name = forms.CharField(label="Full Name", max_length=10, min_length=3)
-    age = forms.IntegerField(label="Own age", validators=[validators.MinValueValidator()])
+    age = forms.IntegerField(label="Own age", validators=[validators.MinValueValidator(15, message="input over 15")])
     mail = forms.EmailField(
         label="Mail address",
         widget=forms.TextInput(attrs={"class": "mail-class", "placeholder": "sample@example.com"}),
