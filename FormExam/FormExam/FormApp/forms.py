ed0304d59c73403b320cf1e45983a3817492b65a
Liberbell <liberbell@gmail.com>
2022-03-17 19:29:06 +0900

    forms2

--- FormExam/FormExam/FormApp/forms.py
+++ FormExam/FormExam/FormApp/forms.py
@@ -1,4 +1,8 @@
 from .models import Students
 from django import forms
 
-class StudentsInsert()
+class StudentsInsert(forms.ModelForm):
+    name = forms.CharField(label="name")
+    age = forms.IntegerField(label="age")
+    grade = forms.IntegerField(label="grade")
+    picture = forms.FileField(label="file_upload")
