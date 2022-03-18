c72921449d4cb97574cbedf530812abddcf22880
Liberbell <liberbell@gmail.com>
2022-03-18 12:09:59 +0900

    views6

--- FormExam/FormExam/FormApp/views.py
+++ FormExam/FormExam/FormApp/views.py
@@ -4,10 +4,10 @@ from . import forms
 # Create your views here.
 
 def insert_student(request):
-    insert_form = forms.StudentsInsertForm(request.POST or None, request.FILES or None)
+    insert_form = forms.StudentInsertForm(request.POST or None, request.FILES or None)
     if insert_form.is_valid():
         insert_form.save()
-        insert_form = forms.StudentsInsertForm()
+        insert_form = forms.StudentInsertForm()
     return render(
         request, "form_app/insert_student.html", context={
             'insert_form': insert_form
