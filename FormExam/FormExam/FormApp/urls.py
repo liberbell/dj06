162a568b2803ac1ec0bb3e74e9a96dab77f4ed1b
Liberbell <liberbell@gmail.com>
2022-03-20 12:44:42 +0900

    urls1

--- FormExam/FormExam/FormApp/urls.py
+++ FormExam/FormExam/FormApp/urls.py
@@ -6,5 +6,5 @@ app_name = "form_app"
 urlpatterns = [
     path("insert_student", views.insert_student, name="insert_student"),
     path("students_list", views.students_list, name="students_list"),
-    path("update_student", views.update_student, name="update_student"),
+    path("update_student/<int:id>", views.update_student, name="update_student"),
 ]
