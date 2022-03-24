6c7131ae7b334c7d56b58d74d4205d1e94ab8396
Liberbell <liberbell@gmail.com>
2022-03-25 08:57:20 +0900

    forms3

--- AdvanceViewSample/user/forms.py
+++ AdvanceViewSample/user/forms.py
@@ -1,3 +1,11 @@
+from dataclasses import fields
 from django import forms
 from django.contrib.auth.models import User
 from user.models import Profile
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput())
+    
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
