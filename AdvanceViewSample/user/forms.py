b92cb98a14abc36fbf146acb5ee0d1bd5b243f43
Liberbell <liberbell@gmail.com>
2022-03-25 08:59:03 +0900

    forms3

--- AdvanceViewSample/user/forms.py
+++ AdvanceViewSample/user/forms.py
@@ -4,8 +4,14 @@ from django.contrib.auth.models import User
 from user.models import Profile
 
 class UserForm(forms.ModelForm):
+    username = forms.CharField(label="Name")
+    email = forms.EmailField(label="E-Mail")
     password = forms.CharField(label="Password", widget=forms.PasswordInput())
     
     class Meta:
         model = User
         fields = ("username", "email", "password")
+
+class ProfileForm(forms.ModelForm):
+    website = forms.URLField(label="HomePage")
+    picture = forms.FileField(label="Picture")
