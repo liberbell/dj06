536e2f349b09639981a12c17b3191b712a4b1ed5
Liberbell <liberbell@gmail.com>
2022-03-25 08:53:54 +0900

    models3

--- AdvanceViewSample/user/models.py
+++ AdvanceViewSample/user/models.py
@@ -7,4 +7,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=CASCADE)
     website = models.URLField(blank=True)
-    picture = models.FileField(upload_to="user/")
+    picture = models.FileField(upload_to="user/", blank=True)
+
+    def __str__(self):
+        return self.user.username
