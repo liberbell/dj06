4f681e1de04768f8fdca5a804045346c3a64ad94
Liberbell <liberbell@gmail.com>
2022-03-25 08:52:58 +0900

    models2

--- AdvanceViewSample/user/models.py
+++ AdvanceViewSample/user/models.py
@@ -7,3 +7,4 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=CASCADE)
     website = models.URLField(blank=True)
+    picture = models.FileField(upload_to="user/")
