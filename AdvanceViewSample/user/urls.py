d6af13ef8b2bcc36adbd2040535dbd0f5a828216
Liberbell <liberbell@gmail.com>
2022-03-27 14:40:27 +0900

    urls1

--- AdvanceViewSample/user/urls.py
+++ AdvanceViewSample/user/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = "user"
 
 urlpatterns = [
-    path("index", views.index, name="index"),
-    path("user_list", views.user_list, name="user_list"),
-    path("register", views.register, name="register"),
+    path("index/", views.index, name="index"),
+    path("user_list/", views.user_list, name="user_list"),
+    path("register/", views.register, name="register"),
+    path("user_login/", views.user_login, name="user_login"),
+    path("user_logout/", views.user_logout, name="user_logout"),
+    path("info/", views.info, name="info"),
 ]
