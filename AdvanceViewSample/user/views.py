d97abf7bf397ff420b9f784ebcaf9f5cd1e6616e
Liberbell <liberbell@gmail.com>
2022-03-27 14:29:11 +0900

    views8

--- AdvanceViewSample/user/views.py
+++ AdvanceViewSample/user/views.py
@@ -35,3 +35,6 @@ def user_login(request):
         if user:
             if user.is_active():
                 login(request, user)
+                return redirect("user:index")
+            else:
+                return HttpResponse("User is not found.")
