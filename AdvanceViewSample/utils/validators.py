cfce7f969becf599a9b15a08f9e504870899945a
Liberbell <liberbell@gmail.com>
2022-03-28 16:28:54 +0900

    validators3

--- AdvanceViewSample/utils/validators.py
+++ AdvanceViewSample/utils/validators.py
@@ -5,3 +5,6 @@ class CustomPasswordValidator():
 
     def __init__(self):
         pass
+
+    def validate(self, password, user=None):
+        if all((re.search('0-9', password), re.search('a-z', password), re.search('A-Z', password)))
