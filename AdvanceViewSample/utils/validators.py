50f93d17febad5edca9a4d141ec06433e415d23a
Liberbell <liberbell@gmail.com>
2022-03-28 16:30:34 +0900

    validators4

--- AdvanceViewSample/utils/validators.py
+++ AdvanceViewSample/utils/validators.py
@@ -1,3 +1,4 @@
+from signal import raise_signal
 from django.core.exceptions import ValidationError
 import re
 
@@ -7,4 +8,6 @@ class CustomPasswordValidator():
         pass
 
     def validate(self, password, user=None):
-        if all((re.search('0-9', password), re.search('a-z', password), re.search('A-Z', password)))
+        if all((re.search('0-9', password), re.search('a-z', password), re.search('A-Z', password))):
+            return
+        raise ValidationError("Require for password 0-9, a-z, A-Z")
