2d5fda7b50e24c6453e7926b7ec502f2dbe0f03d
Liberbell <liberbell@gmail.com>
2022-03-28 09:20:30 +0900

    views4

--- AdvanceViewSample/user/views.py
+++ AdvanceViewSample/user/views.py
@@ -1,9 +1,10 @@
-from wsgiref.util import request_uri
 from django.shortcuts import render, redirect
 from user.forms import UserForm, ProfileForm, LoginForm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 def user_list(request):
@@ -16,7 +17,9 @@ def register(request):
     user_form = UserForm(request.POST or None)
     profile_form = ProfileForm(request.POST or None, request.FILES or None)
     if user_form.is_valid() and profile_form.is_valid():
-        user = user_form.save()
+        user = user_form.save(commit=False)
+        try:
+            validate_password(user_form.cleaned_data.get("password"), user)
         user.set_password(user.password)
         user.save()
         profile = profile_form.save(commit=False)
