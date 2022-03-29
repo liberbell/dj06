a3acb7ee383d1e8fd97576231db93dff9a457938
Liberbell <liberbell@gmail.com>
2022-03-30 08:58:13 +0900

    models7

--- CustomizeUser/accounts/models.py
+++ CustomizeUser/accounts/models.py
@@ -40,7 +40,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     picture = models.FileField(null=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['username']
+    REQUIRED_FIELDS = ['username']
 
     objects = UserManager()
 
