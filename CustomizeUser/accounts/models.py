1508dbc1e8af633d33802a5a9434e381fbb40039
Liberbell <liberbell@gmail.com>
2022-03-30 08:49:50 +0900

    models5

--- CustomizeUser/accounts/models.py
+++ CustomizeUser/accounts/models.py
@@ -6,7 +6,8 @@ from django.contrib.auth.models import (
 
 # Create your models here.
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None)
+
+    def create_user(self, username, email, password=None):
     if not email:
         raise ValueError("Email not found")
     user = self.model(
@@ -14,7 +15,10 @@ class UserManager(BaseUserManager):
         email = email
     )
     user.set_password(password)
-    user.save()
+    user.save(using=self._db)
+    return user
+
+    def create_superuser(self, username, email, password=None):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
