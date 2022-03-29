721a9425c62b86b3c0e4fd70f970b7c3a398ec3d
Liberbell <liberbell@gmail.com>
2022-03-29 09:11:11 +0900

    models3

--- CustomizeUser/accounts/models.py
+++ CustomizeUser/accounts/models.py
@@ -10,3 +10,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    website = models.URLField(null=True)
+    picture = models.FileField(null=True)
