df2e70ebfaa7eecab62ad88aa3d2c9d047bb04b5
Liberbell <liberbell@gmail.com>
2022-03-30 08:54:56 +0900

    settings1

--- CustomizeUser/CustomizeUser/settings.py
+++ CustomizeUser/CustomizeUser/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CustomizeUser.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
