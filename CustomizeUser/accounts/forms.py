ec13f125b173e75cb7fae3b616e1472a4b9567da
Liberbell <liberbell@gmail.com>
2022-03-30 09:09:35 +0900

    forms3

--- CustomizeUser/accounts/forms.py
+++ CustomizeUser/accounts/forms.py
@@ -1,7 +1,10 @@
-from socket import fromshare
-from django import froms
+from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 
+User = get_user_model()
 
+class UserCreationForm(forms.ModelForm):
+    Password = forms.CharField(label="password", widget=forms.PasswordInput)
+    Confirm_password = forms.CharField(label="confirm password", widget=forms.PasswordInput)
