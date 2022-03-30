0badc0e474d7aa883ca945be27939fca20ff808f
Liberbell <liberbell@gmail.com>
2022-03-30 09:04:09 +0900

    forms2

--- CustomizeUser/accounts/forms.py
+++ CustomizeUser/accounts/forms.py
@@ -1,3 +1,7 @@
 from socket import fromshare
 from django import froms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+
