b1e545449386dea5835be15742a6c7cd01bf2cc5
Liberbell <liberbell@gmail.com>
2022-03-31 09:01:08 +0900

    admin2

--- CustomizeUser/accounts/admin.py
+++ CustomizeUser/accounts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
+from .forms import UserChangeForm, UserCreationForm
 
 # Register your models here.
+user = get_user_model()
+
+class CustomizeUserAdmin(UserAdmin):
+    form = UserChangeForm
