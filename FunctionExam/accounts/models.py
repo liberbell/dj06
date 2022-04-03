c590b81d6e82e1a13450935a33ad58261ce14b58
Liberbell <liberbell@gmail.com>
2022-04-03 14:44:28 +0900

    models1

--- FunctionExam/accounts/models.py
+++ FunctionExam/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin
+)
 
 # Create your models here.
+class Users(AbstractBaseUser, PermissionsMixin):
+    
