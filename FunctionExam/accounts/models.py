59db992d52a5b63747bd00edfc03888f60dee59b
Liberbell <liberbell@gmail.com>
2022-04-06 08:51:58 +0900

    models3

--- FunctionExam/accounts/models.py
+++ FunctionExam/accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser, PermissionsMixin
 )
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Users(AbstractBaseUser, PermissionsMixin):
