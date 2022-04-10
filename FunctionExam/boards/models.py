14ea5ede82900f91e1a7c891b607fa668622fae6
Liberbell <liberbell@gmail.com>
2022-04-10 13:06:06 +0900

    models2

--- FunctionExam/boards/models.py
+++ FunctionExam/boards/models.py
@@ -8,3 +8,17 @@ class Themes(models.Model):
     user = models.ForeignKey(
         "accounts.Users", on_delete=models.CASCADE
     )
+
+    class Meta:
+        db_table = "themes"
+
+class Comments(models.Model):
+
+    comment = models.CharField(max_length=1000)
+    user = models.ForeignKey(
+        "accounts.User", on_delete=models.CASCADE
+    )
+    theme = models.ForeignKey("Themes", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "comments"
