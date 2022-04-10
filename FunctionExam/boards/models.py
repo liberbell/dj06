635b989902ed4f431cc4c6a3f54191e82df33426
Liberbell <liberbell@gmail.com>
2022-04-10 13:03:03 +0900

    models1

--- FunctionExam/boards/models.py
+++ FunctionExam/boards/models.py
@@ -1,3 +1,10 @@
+from turtle import ondrag
 from django.db import models
 
 # Create your models here.
+class Themes(models.Model):
+
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        "accounts.Users", on_delete=models.CASCADE
+    )
