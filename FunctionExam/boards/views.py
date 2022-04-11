cabfb32773b3903bd6a2df4e572c75611257a59a
Liberbell <liberbell@gmail.com>
2022-04-12 08:43:22 +0900

    views1

--- FunctionExam/boards/views.py
+++ FunctionExam/boards/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
+def create_theme(request):
+    create_theme_form = forms.CreateThemeForm(request.POST or None)
