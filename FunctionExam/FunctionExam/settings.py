c5ffa478601432770abcf6adf16610c34dfb023e
Liberbell <liberbell@gmail.com>
2022-04-16 10:32:56 +0900

    settings1

--- FunctionExam/FunctionExam/settings.py
+++ FunctionExam/FunctionExam/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-PASSWORD_HASH = [
+PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
