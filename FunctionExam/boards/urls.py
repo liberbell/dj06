5fbd46d9e4c8079b687ba04635546ae1db3df92b
Liberbell <liberbell@gmail.com>
2022-04-16 10:28:16 +0900

    urls1

--- FunctionExam/boards/urls.py
+++ FunctionExam/boards/urls.py
@@ -1,8 +1,11 @@
 from unicodedata import name
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "boards"
+
 
 urlpatterns = [
     path("create_theme", views.create_theme, name="create_theme"),
