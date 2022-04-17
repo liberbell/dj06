afdcd729753c80101dd4e2bd0183093e32135369
Liberbell <liberbell@gmail.com>
2022-04-17 12:48:14 +0900

    settings2

--- FunctionExam/FunctionExam/settings.py
+++ FunctionExam/FunctionExam/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = (
     STATIC_DIR,
 )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
