711bebee4129fe051cf780c5ac25eb2ddeb07974
Liberbell <liberbell@gmail.com>
2022-04-19 08:34:10 +0900

    urls1

--- FunctionExam/FunctionExam/urls.py
+++ FunctionExam/FunctionExam/urls.py
@@ -13,16 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from cgitb import handler
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from boards.views import show_error_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
     path('boards/', include("boards.urls")),
 ]
+handler404
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
