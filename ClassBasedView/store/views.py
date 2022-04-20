f59ca8548d826f16dfaa66e2cb4b15f619645b44
Liberbell <liberbell@gmail.com>
2022-04-20 09:13:24 +0900

    views4

--- ClassBasedView/store/views.py
+++ ClassBasedView/store/views.py
@@ -12,3 +12,11 @@ class IndexView(View):
         return render(request, "index.html", context={
             "book_form": book_form,
         })
+
+    def post(self, request, *args, **kwargs):
+        book_form = forms.BookForm(request.POST or None)
+        if book_form.is_valid():
+            book_form.save()
+            return render(request, "index.html", context={
+                "book_form": book_form,
+            })
