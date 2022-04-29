d42e36ba0911f68aac499575fc3fb8d151fca0b1
Liberbell <liberbell@gmail.com>
2022-04-30 08:43:38 +0900

    forms1

--- ClassBaseLoginView/accounts/forms.py
+++ ClassBaseLoginView/accounts/forms.py
@@ -7,7 +7,7 @@ class RegistForm(forms.ModelForm):
     username = forms.CharField(label="Name")
     age = forms.IntegerField(label="Age", min_value=0)
     email = forms.EmailField(label="E-mail")
-    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput())
 
     class Meta:
         model = Users
