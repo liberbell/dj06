724c73cf1fc7de37eb58b5d4c1223e0f47dd1dfc
Liberbell <liberbell@gmail.com>
2022-04-30 08:29:15 +0900

    urls2

--- ClassBaseLoginView/accounts/urls.py
+++ ClassBaseLoginView/accounts/urls.py
@@ -2,3 +2,11 @@ from django.urls import path
 from .views import (
     RegistUserView, UserLoginView, HomeView, UserLogoutView
 )
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("home/", HomeView.as_view, name="home"),
+    path("regist/", RegistUserView.as_view, name="regist"),
+    path("user_login/", UserLoginView.as_view, name="user_login"),
+]
