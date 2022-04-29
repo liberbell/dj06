f6ba8a53421de6ea534ed76c07844fcd64b55295
Liberbell <liberbell@gmail.com>
2022-04-29 21:15:20 +0900

    forms4

--- ClassBaseLoginView/accounts/forms.py
+++ ClassBaseLoginView/accounts/forms.py
@@ -14,4 +14,8 @@ class RegistForm(forms.ModelForm):
         fields = ["username", "age", "email", "password"]
 
     def save(self, commit=False):
-        user = super().save(commit=False)
+        username = super().save(commit=False)
+        validate_password(self.cleaned_data["password"], user)
+        user.set_password(self.cleaned_data["password"])
+        user.save()
+        return user
