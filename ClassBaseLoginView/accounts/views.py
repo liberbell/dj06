1e7eea108069cdc11cd958ee596fd377ab4f6f05
Liberbell <liberbell@gmail.com>
2022-05-03 13:04:09 +0900

    views3

--- ClassBaseLoginView/accounts/views.py
+++ ClassBaseLoginView/accounts/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
 class HomeView(TemplateView):
