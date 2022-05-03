4096165c371387a68fbadef0f2bd1b0ab722fd55
Liberbell <liberbell@gmail.com>
2022-05-03 13:10:19 +0900

    django4

--- ClassBaseLoginView/ClassBaseLoginView/settings.py
+++ ClassBaseLoginView/ClassBaseLoginView/settings.py
@@ -128,4 +128,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/accounts/user_login"
-LOGIN_REDIRECTURL = "/accounts/home"
+LOGIN_REDIRECT_URL = "/accounts/home"
