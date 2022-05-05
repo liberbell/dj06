8f5efe8d57ba73dfb9a18ad8db0f2060dc7c6ad6
Liberbell <liberbell@gmail.com>
2022-05-05 17:52:50 +0900

    02logging7

--- LoggingSample/LoggingSample/02_logging_sample.py
+++ LoggingSample/LoggingSample/02_logging_sample.py
@@ -14,6 +14,12 @@ f_handler.setLevel(logging.ERROR)
 s_formatter = logging.Formatter("%(name)s-%(levelname)s-%(message)s")
 f_formatter = logging.Formatter("%(asctime)s-%(name)s-%(levelname)s-%(message)s")
 
+s_handler.setFormatter(s_formatter)
+f_handler.setFormatter(f_formatter)
+
+logger.addHandler(s_handler)
+logger.addHandler(f_handler)
+
 logger.debug("debug log")
 logger.info("info log")
 logger.warning("warning log")
