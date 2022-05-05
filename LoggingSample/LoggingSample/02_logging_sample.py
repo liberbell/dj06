847f6ca25163fce266e3a6dd7165d21ab37d31ac
Liberbell <liberbell@gmail.com>
2022-05-05 17:45:33 +0900

    02logging1

--- LoggingSample/LoggingSample/02_logging_sample.py
+++ LoggingSample/LoggingSample/02_logging_sample.py
@@ -2,3 +2,10 @@ from asyncio.log import logger
 import logging
 
 logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.DEBUG)
+logger.debug("debug log")
+logger.info("info log")
+logger.warning("warning log")
+logger.error("error log")
+logger.critical("critical log")
