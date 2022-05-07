4965302f23a5611b4e9880a11a266b4692cc43ae
Liberbell <liberbell@gmail.com>
2022-05-07 09:44:32 +0900

    views2

--- ClassBasedView/store/views.py
+++ ClassBasedView/store/views.py
@@ -51,6 +51,7 @@ class BookDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        application_logger("dispaly home page.")
         # print(context)
         # context["form"] = forms.BookForm()
         return context
