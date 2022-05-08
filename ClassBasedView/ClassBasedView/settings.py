c9adb4d115fb7aad0c7fc9aea840cac01b80283c
Liberbell <liberbell@gmail.com>
2022-05-09 08:34:33 +0900

    settings1

--- ClassBasedView/ClassBasedView/settings.py
+++ ClassBasedView/ClassBasedView/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'class_based_view.middleware.MyMiddleWare',
 ]
 
 ROOT_URLCONF = 'ClassBasedView.urls'
