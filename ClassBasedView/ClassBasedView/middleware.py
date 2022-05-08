6bb0178c6f51b646364c8b5575bbf17d47f547e9
Liberbell <liberbell@gmail.com>
2022-05-09 08:39:02 +0900

    middleware4

--- ClassBasedView/ClassBasedView/middleware.py
+++ ClassBasedView/ClassBasedView/middleware.py
@@ -2,6 +2,7 @@ import logging
 from django.utils.deprecation import MiddlewareMixin
 
 application_logger = logging.getLogger("application-logger")
+error_logger = logging.getLogger("error-logger")
 
 class MyMiddleWare(MiddlewareMixin):
     
@@ -9,3 +10,5 @@ class MyMiddleWare(MiddlewareMixin):
         application_logger.info(request.get_full_path())
         # print(dir(request))
 
+    def process_exception(self, request, exception):
+        error_logger.error(exception, )
