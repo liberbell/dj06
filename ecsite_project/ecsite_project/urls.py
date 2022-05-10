f13d41371d2839bc9f59f801c674461063f433a0
Liberbell <liberbell@gmail.com>
2022-05-11 08:53:19 +0900

    urls1

--- ecsite_project/ecsite_project/urls.py
+++ ecsite_project/ecsite_project/urls.py
@@ -1,7 +1,13 @@
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, documet_root=settings.MEDIA_ROOT)
