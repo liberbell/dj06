79a3740777d87f2f14fd5b6b704ece35691a8b42
Liberbell <liberbell@gmail.com>
2022-05-11 08:44:17 +0900

    admin1

--- ecsite_project/stores/admin.py
+++ ecsite_project/stores/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import (
+    ProductTypes, Manufacturers, Products, ProductPictures
+)
 
 # Register your models here.
+admin.site.register(
+    [ProductTypes, Manufacturers, Products, ProductPictures]
+)
