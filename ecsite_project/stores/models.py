ed2dca4ba13d666c516853b4475c6916c4f6311a
Liberbell <liberbell@gmail.com>
2022-05-10 09:19:24 +0900

    models1

--- ecsite_project/stores/models.py
+++ ecsite_project/stores/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ProductTypes(models.Model):
+    name = models.CharField(max_length=1000)
+
+    class Meta:
+        db_table = "product_types"
