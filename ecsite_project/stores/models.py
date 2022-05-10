67628e81b3fbc144faef53fcc5d18f922fda7972
Liberbell <liberbell@gmail.com>
2022-05-10 09:22:13 +0900

    models2

--- ecsite_project/stores/models.py
+++ ecsite_project/stores/models.py
@@ -6,3 +6,15 @@ class ProductTypes(models.Model):
 
     class Meta:
         db_table = "product_types"
+
+    def __str__(self):
+        return self.name
+
+class Manufacturers(models.Model):
+    name = models.CharField(max_length=1000)
+
+    class Meta:
+        db_table = "manufacturers"
+
+    def __str__(self):
+        return self.name
