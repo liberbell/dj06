3e4a213ba735c121474d1742229a162cf58d91bc
Liberbell <liberbell@gmail.com>
2022-05-12 08:53:55 +0900

    settings1

--- ecsite_project/ecsite_project/settings.py
+++ ecsite_project/ecsite_project/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 
 LOGIN_URL = '/accounts/user_login'
 LOGIN_REDIRECT_URL = '/accounts/home'
 LOGOUT_REDIRECT_URL = '/accounts/user_login'
 
-SESSION_COOKIE_AGE = 5
+# SESSION_COOKIE_AGE = 5
