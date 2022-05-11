30586bae133f840409c1f1df216f8b8032795be5
Liberbell <liberbell@gmail.com>
2022-05-12 08:50:25 +0900

    models4

--- ecsite_project/stores/models.py
+++ ecsite_project/stores/models.py
@@ -31,3 +31,15 @@ class Products(models.Model):
     
     def __str__(self):
         return self.name
+
+class ProductPictures(models.Model):
+    picture = models.FileField(upload_to="product_pictures/")
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    order = models.IntegerField()
+
+    class Meta:
+        db_table = "product_pictures"
+        ordering = ["order"]
+    
+    def __str__(self):
+        return self.product.name + ": " + str(self.order)
