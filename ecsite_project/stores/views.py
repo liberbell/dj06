ce376dac6b5ef228889407687665c7e1290baea8
Liberbell <liberbell@gmail.com>
2022-05-13 08:48:50 +0900

    views1

--- ecsite_project/stores/views.py
+++ ecsite_project/stores/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+import os
+from .models import (
+    Products
+)
 
 # Create your views here.
+class ProductListView(LoginRequiredMixin, ListView):
+    model = Products
