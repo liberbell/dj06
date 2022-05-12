0e0da6f51a9997d6210e5079a9bcba59c868fab1
Liberbell <liberbell@gmail.com>
2022-05-13 08:51:18 +0900

    urls2

--- ecsite_project/stores/urls.py
+++ ecsite_project/stores/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import ProductListView
 
 app_name = "stores"
 urlpatterns = [
-    path("", ),
+    path("product_list/", ProductListView.as_view(), name="product_list"),
 ]
