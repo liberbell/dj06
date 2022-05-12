2f5674239cf8a518ba199470e317178cba91308b
Liberbell <liberbell@gmail.com>
2022-05-13 08:38:01 +0900

    models2

--- ecsite_project/accounts/models.py
+++ ecsite_project/accounts/models.py
@@ -25,7 +25,7 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.is_staff = True
         user.is_active = True
-        user.is_supersuer = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
