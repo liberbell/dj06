adc44e85f4b950cd1c2a7c422b8968ea3a3e63dd
Liberbell <liberbell@gmail.com>
2022-05-19 22:40:49 +0900

    urls2

--- ecsite_project/stores/urls.py
+++ ecsite_project/stores/urls.py
@@ -5,5 +5,5 @@ app_name = "stores"
 urlpatterns = [
     path("product_list/", ProductListView.as_view(), name="product_list"),
     path("product_detail/<int:pk>", ProductDetailView.as_view(), name="product_detail"),
-    path("add_product/<int:pk>", add_product.as_view(), name="add_product"),
+    path("add_product/", add_product, name="add_product"),
 ]
