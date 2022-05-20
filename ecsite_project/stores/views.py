104afc47999f21ce19afe502b1d98b0564f7f804
Liberbell <liberbell@gmail.com>
2022-05-20 12:23:58 +0900

    views1

--- ecsite_project/stores/views.py
+++ ecsite_project/stores/views.py
@@ -1,4 +1,3 @@
-from urllib import response
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
