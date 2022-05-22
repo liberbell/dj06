50bc59b86985848c379a9fac93cea0c1c9b9489b
Liberbell <liberbell@gmail.com>
2022-05-23 08:58:57 +0900

    forms4

--- ecsite_project/stores/forms.py
+++ ecsite_project/stores/forms.py
@@ -2,6 +2,7 @@ from xml.dom import ValidationErr
 from django import forms
 from .models import CartsItem
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
 
 class CartUpdateForm(forms.ModelForm):
     quantity = forms.IntegerField(label="stocks", min_value=1)
@@ -18,6 +19,4 @@ class CartUpdateForm(forms.ModelForm):
             id = cleand_data.get("id")
             cart_item = get_object_or_404(CartsItem, pk=id)
             if quantity > cart_item.product.stock:
-                raise ValidationErr(f"Over stocks. Input under {cart_item.product.stock}.")
-            return data
-        
+                raise ValidationError(f"Over stocks. Input under {cart_item.product.stock}.")
