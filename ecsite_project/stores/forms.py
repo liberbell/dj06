d29188e051c344b6b664f31185543f733a05703a
Liberbell <liberbell@gmail.com>
2022-05-23 08:51:46 +0900

    forms2

--- ecsite_project/stores/forms.py
+++ ecsite_project/stores/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import CartsItem
 
 class CartUpdateForm(forms.ModelForm):
-    quantity = forms.IntegerField()
+    quantity = forms.IntegerField(label="stocks", min_value=1)
     id = forms.CharField(widget=forms.HiddenInput)
     
     class Meta:
