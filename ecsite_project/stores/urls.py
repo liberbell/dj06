29643c1e38b9d4b9e7f403b9c8cfce1474399b0a
Liberbell <liberbell@gmail.com>
2022-05-23 09:02:37 +0900

    urls1

--- ecsite_project/stores/urls.py
+++ ecsite_project/stores/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView, add_product, CartItemView
+from .views import ProductListView, ProductDetailView, add_product, CartItemView, CartUpdateView, CartDeleteView
 
 app_name = "stores"
 urlpatterns = [
